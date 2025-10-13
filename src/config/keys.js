a215ae7c135adce4fb72260c09473aa4b518ff71
ruy <duytrandt04@gmail.com>
2025-10-13 21:33:56 +0700

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 export const app = {
     name: "AHA-Capstone",
-    dataURL: http://35.225.246.253:8000,
+    dataURL: "http://35.225.246.253:8000",
     googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
 };
