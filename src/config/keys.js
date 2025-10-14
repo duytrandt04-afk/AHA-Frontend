e3f2ae6656408e5a420b86018991c1df19c66cc3
ruy <duytrandt04@gmail.com>
2025-10-14 18:15:22 +0700

    Update keys.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,5 @@
 export const app = {
     name: "AHA-Capstone",
     dataURL: "https://darrell-gladiolar-jair.ngrok-free.dev",
-    googleClientId: import.meta.env.VITE_GOOGLE_CLIENT_ID,
+    googleClientId: "239662288538-333k92ipd7la1i8s7je5cgdpn4lkigga.apps.googleusercontent.com",
 };
